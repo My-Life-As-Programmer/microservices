f02ffcd8c964358bf17d57ccddeec0e9c1a62b74
My-Life-As-Programmer <krishnachaitanyag24@gmail.com>
2022-06-04 18:10:40 +0530

    Adding skeleton for ProductViewSet

--- products_list/views.py
+++ products_list/views.py
@@ -1,4 +1,18 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
 
+class ProductViewSet(viewsets.ViewSet):
+    def list(self, request):
+        pass
 
+    def create(self, request):
+        pass
+
+    def retrieve(self, request, pk=None):
+        pass
+
+    def update(self, request, pk=None):
+        pass
+
+    def destroy(self, request, pk=None):
+        pass
