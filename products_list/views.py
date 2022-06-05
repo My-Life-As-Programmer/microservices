1ec18849046292e6d9799aabc98cd3428bab1e5d
My-Life-As-Programmer <krishnachaitanyag24@gmail.com>
2022-06-06 01:17:13 +0530

    Updated Response to follow JSend format

--- products_list/views.py
+++ products_list/views.py
@@ -10,32 +10,32 @@ class ProductViewSet(viewsets.ViewSet):
     def list(request):
         products = Product.objects.all()
         serializer = ProductSerializer(products, many=True)
-        return Response({"message": "All Products",
-                         "payload": serializer.data}, status=status.HTTP_200_OK)
+        return Response({"status": "success",
+                         "data": {"products": serializer.data}}, status=status.HTTP_200_OK)
 
     def create(self, request):
         serializer = ProductSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
-        return Response({"message": "Product Created",
-                         "payload": serializer.data}, status=status.HTTP_201_CREATED)
+        return Response({"status": "success",
+                         "data": {"product": serializer.data}}, status=status.HTTP_201_CREATED)
 
     def retrieve(self, request, pk=None):
         product = Product.objects.get(id=pk)
         serializer = ProductSerializer(product)
-        return Response({"message": "Product Retrieved",
-                         "payload": serializer.data}, status=status.HTTP_200_OK)
+        return Response({"status": "success",
+                         "data": {"product": serializer.data}}, status=status.HTTP_200_OK)
 
     def update(self, request, pk=None):
         product = Product.objects.get(id=pk)
         serializer = ProductSerializer(instance=product, data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
-        return Response({"message": "Product Updated",
-                         "payload": serializer.data}, status=status.HTTP_202_ACCEPTED)
+        return Response({"status": "success",
+                         "data": {"product": serializer.data}}, status=status.HTTP_202_ACCEPTED)
 
     def destroy(self, request, pk=None):
         product = Product.objects.get(id=pk)
         Product.delete(product)
-        return Response({"message": "Product Deleted",
-                         "payload": None}, status=status.HTTP_204_NO_CONTENT)
+        return Response({"status": "success",
+                         "data": None}, status=status.HTTP_204_NO_CONTENT)
