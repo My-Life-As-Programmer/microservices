a714985a758ab9aa7bfaa0e34ca06289961bc62f
My-Life-As-Programmer <krishnachaitanyag24@gmail.com>
2022-06-12 02:21:57 +0530

    Testing Flask 2.0 CLI commands

--- main/main.py
+++ main/main.py
@@ -1,4 +1,7 @@
+import click
+
 from flask import Flask
+from flask.cli import with_appcontext
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import UniqueConstraint
@@ -24,10 +27,32 @@ class ProductUser(db.Model):
     UniqueConstraint('user_id', 'product_id', name='user_product_unique')
 
 
+@click.command(name='create')
+@with_appcontext
+def create():
+    db.create_all()
+
+
+@click.command(name='db_init')
+@with_appcontext
+def db_init():
+    db.init()
+
+
+@click.command(name='db_migrate')
+@with_appcontext
+def db_migrate():
+    db.migrate()
+
+
 @app.route('/')
 def index():
     return 'Hello'
 
 
+app.cli.add_command(create)
+app.cli.add_command(db_init)
+app.cli.add_command(db_migrate)
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    # app.run(debug=True, host='0.0.0.0')
+    pass
--- main/manager.py
+++ main/manager.py
@@ -1,11 +1,13 @@
-from main import app, db
+from flask.cli import FlaskGroup
 from flask_migrate import Migrate, MigrateCommand
-from flask_script import Manager
 
-migrate = Migrate(app, db)
+from main import app, db
 
-manager = Manager(app)
-manager.add_command('db', MigrateCommand)
+migrate = Migrate()
+migrate.init_app(app, db)
+
+cli = FlaskGroup(app)
+# cli.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
-    manager.run()
+    cli()
